2969bcf402df4bf21498afeefdc7782d5761fa86
Lisa Xie <lisaxie3001@gmail.com>
2023-11-04 01:47:57 -0400

    WIP

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+Creating REST APIs using Flask: 
+- https://auth0.com/blog/developing-restful-apis-with-python-and-flask/#Summary
+- https://www.freecodecamp.org/news/structuring-a-flask-restplus-web-service-for-production-builds-c2ec676de563/
+
+Adding authentication: 
+- https://auth0.com/docs/quickstart/backend/python/interactive
---
+++ backend/app/main/controller/controller.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+# Testing
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
