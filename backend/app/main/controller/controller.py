cb82707888fbdfcf91402eb132c6399a26c9e63e
Lisa Xie <lisaxie3001@gmail.com>
2024-03-18 19:53:17 -0400

    Hardcoded demo image

--- backend/app/main/controller/controller.py
+++ backend/app/main/controller/controller.py
@@ -12,33 +12,87 @@ from app.main.service import (
 
 cache = cache_service.Cache_Service()
 model_service = model_integration_service.Model_Integration_Service()
+emr_service = emr_integration_service.EMR_Integration_Service()
+
+DEMO_CASES = [
+    {
+        "filename": "ISIC_4577414.jpg",
+        "sex": "male",
+        "age_approx": "60",
+        "anatom_site_general_challenge": "torso",
+        "diagnosis": "unknown",
+        "benign_malignant": "benign",
+    },
+    {
+        "filename": "ISIC_3747575.jpg",
+        "sex": "male",
+        "age_approx": "45",
+        "anatom_site_general_challenge": "torso",
+        "diagnosis": "unknown",
+        "benign_malignant": "benign",
+    },
+    {
+        "filename": "ISIC_4434221.jpg",
+        "sex": "male",
+        "age_approx": "30",
+        "anatom_site_general_challenge": "torso",
+        "diagnosis": "unknown",
+        "benign_malignant": "benign",
+    },
+    {
+        "filename": "ISIC_4577414.jpg",
+        "sex": "male",
+        "age_approx": "60",
+        "anatom_site_general_challenge": "lower extremity",
+        "diagnosis": "unknown",
+        "benign_malignant": "benign",
+    },
+]
 
 
 # Returns all cases in last 5 min
 @controller_endpoints.route("/cases", methods=["GET"])
 def cases():
+    response = []
+
+    keys = cache.get()
+    print(keys)
+    response.append(
+        {
+            "image": base64.b64encode(open("ISIC_0490442.JPG", "rb").read()).decode(
+                "utf-8"
+            ),
+            "cases": DEMO_CASES,
+        }
+    )
+    for key in keys:
+        response.append(
+            {
+                "image": cache.images[key],
+                "cases": cache.cases[key],
+            }
+        )
+
+    return jsonify(response)
+
+
+@controller_endpoints.route("/cases_testing", methods=["GET"])
+def cases_testing():
     data = request.json
-    # print(cache.images)
     response = {}
 
     keys = cache.get()
     print(keys)
 
-    for key in keys:
-        response[key] = {
-            "image": cache.images[key],
-            "cases": cache.cases[key],
-        }
-
     # hard-coded response for testing
-    # for key in range(2):
-    #     response[key] = {
-    #         "image": "byte_data",
-    #         "cases": [
-    #             "/static/images/ISIC_0015719.jpg",
-    #             "/static/images/ISIC_0052212.jpg",
-    #         ],
-    #     }
+    for key in range(2):
+        response[key] = {
+            "image": "byte_data",
+            "cases": [
+                "/static/images/ISIC_0015719.jpg",
+                "/static/images/ISIC_0052212.jpg",
+            ],
+        }
 
     return jsonify(response)
 
@@ -76,9 +130,25 @@ def clear():
 # return jsonify(response)
 
 
-# Returns most recent case id
+# Mobile endpoint: receive image of mole from mobile app
 @controller_endpoints.route("/image", methods=["POST"])
 def image():
+    data = request.json
+
+    # for testing
+    with open("imageToSave.jpg", "wb") as fh:
+        fh.write(base64.b64decode(data["image"]))
+
+    image = data["image"]
+    cases = model_service.evaluate()
+
+    cache.add(image, cases)
+
+    return jsonify({})
+
+
+@controller_endpoints.route("/upload_to_emr", methods=["POST"])
+def upload_to_emr():
     data = request.json
 
     # for testing
